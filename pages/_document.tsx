fc4367bd842608f3ab9546358562a08744103763
Shlokova Anna <shlokova777@gmail.com>
2022-12-17 10:35:26 +0300

    поправила  head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,14 +8,15 @@ export default function Document() {
         <meta name="description" content={'frontend developer'} />
         <meta name="keywords" content={'frontend, frontend developer, CV, resume, Shlokova, Anna, Shlokova Anna'} />
         <meta property="og:image" content="/share.webp" />
-        <meta name="twitter:image" content="/share.webp" />
-        <meta name="twitter:card" content="summary_large_image" />
         <meta property="og:url" content="https://shlokova-cv.web.app/" />
         <meta property="og:title" content="Shlokova Anna" />
         <meta property="og:description" content="frontend developer" />
         <meta property="og:site_name" content="CV" />
         <meta property="og:type" content="website" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="twitter:title" content="Shlokova Anna" />
+        <meta name="twitter:description" content="frontend developer" />
+        <meta name="twitter:image" content="/share.webp" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="manifest" href="/static/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/static/icon.png" />
